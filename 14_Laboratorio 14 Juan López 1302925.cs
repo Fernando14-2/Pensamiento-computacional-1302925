15d536b6a4b6ce1c9de08590c8c8974ef8f5a7d0
Fernando14-2 <juanferlopez2005@gmail.com>
2025-05-21 00:34:47 -0600

    Create 14_Laboratorio 14 Juan López 1302925.cs

---
+++ 14_Laboratorio 14 Juan López 1302925.cs
@@ -0,0 +1,34 @@
+﻿
+/* ejemplo arreglos
+Console.WriteLine("Ejemplo de arreglos");
+
+String[] nombres = ["Juan", "Pedro", "Luisa", "Adriana", "Sofia"];
+string[] apellidos = new string[5];
+
+string nombre = nombres[3];
+for (int i = 0; i < nombres.Length; i++)
+{
+    Console.WriteLine($"nombre no, {i}: " + nombres[i]);
+    apellidos[i] = Console.ReadLine();
+}
+
+for (int i = 0; i < apellidos.Length; i++)
+{
+    Console.WriteLine($"apellido no. {i}: " + apellidos[i]);
+}
+*/
+
+//Juan Fernando López Cobo
+string[] estudiantes = ["Juan", "Pedro", "Luisa", "Adriana", "Sofia"];
+int[] notas = [88, 75, 96, 77, 59];
+int promedio = 0;
+
+
+for (int i = 0; i < estudiantes.Length; i++)
+{
+    Console.WriteLine(estudiantes[i] + " - " + notas[i]);
+    promedio += notas[i];
+}
+
+int promedioFinal = promedio/notas.Length;
+Console.WriteLine($"El promedio de los estudiantes es de {promedioFinal}");
